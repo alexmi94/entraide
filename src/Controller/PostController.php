ee075ff2c9f723f23f2bd727c70303f88bab83e1
alexmi94 <alexandrecance94@gmail.com>
2023-06-29 15:12:00 +0200

    bugfix

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -6,15 +6,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Post;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Security\Core\Security;
+use Doctrine\ORM\EntityManagerInterface;
 
 class PostController extends AbstractController
 {
     private $entityManager;
+    private $security;
 
 
-    public function __construct(ManagerRegistry $managerRegistry, ReservationRepository $reservationRepository, Security $security, EntityManagerInterface $entityManager)
+    public function __construct(ManagerRegistry $managerRegistry, Security $security, EntityManagerInterface $entityManager)
     {
         $this->entityManager = $entityManager;
+        $this->security = $security;
     }
 
 
