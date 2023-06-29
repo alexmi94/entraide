baa724aee098802c92c558456b67749403009bf4
alexmi94 <alexandrecance94@gmail.com>
2023-06-29 15:44:30 +0200

    mon compte

--- src/Controller/MonCompteController.php
+++ src/Controller/MonCompteController.php
@@ -5,14 +5,34 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Security\Core\Security;
+use Doctrine\ORM\EntityManagerInterface;
 
 class MonCompteController extends AbstractController
 {
+
+    private $entityManager;
+    private $security;
+    private $managerRegistry;
+
+
+    public function __construct(ManagerRegistry $managerRegistry, Security $security, EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+        $this->security = $security;
+        $this->managerRegistry = $managerRegistry;
+    }
+
     #[Route('/compte', name: 'app_compte')]
     public function index(): Response
     {
+
+        $user = $this->security->getUser();
+
+
         return $this->render('compte/index.html.twig', [
-            'controller_name' => 'MonCompteController',
+            'user' => $user,
         ]);
     }
 }
