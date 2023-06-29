e54bd75ea371b7a7499547c1036fef70baf5d5a1
alexmi94 <alexandrecance94@gmail.com>
2023-06-29 16:17:28 +0200

    home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,31 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Post;
 
 class HomeController extends AbstractController
 {
+    private $entityManager;
+    private $managerRegistry;
+
+
+    public function __construct(ManagerRegistry $managerRegistry, EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+        $this->managerRegistry = $managerRegistry;
+    }
+
+
     #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
+
+        $posts = $this->entityManager->getRepository(Post::class)->findAll();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'posts' => $posts ,
         ]);
     }
 }
--- src/Controller/RechercheController.php
+++ src/Controller/RechercheController.php
@@ -10,16 +10,36 @@ use Symfony\Component\Security\Core\Security;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use App\Entity\Post;
+use App\Form\RechercheType;
+
 
 class RechercheController extends AbstractController
 {
+    private $entityManager;
+
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/recherche', name: 'app_recherche')]
-    public function index(): Response
+    public function index(Request $request): Response
     {   
         
+        $form = $this->createForm(RechercheType::class);
+        $form->handleRequest($request);
+
+        $Post = [];
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $titre = $form->getData()['titre'];
+            $Post = $this->entityManager->getRepository(Post::class)->findBy(['titre' => $titre]);
+        }
 
         return $this->render('recherche/index.html.twig', [
-            'controller_name' => 'RechercheController',
+            'post' => $post,
         ]);
     }
 }
